3dbe07218af2e6ddb9e08d07b61c35a259dccef0
Marcin Woźnica <poczta@marcin.wroclaw.pl>
2025-02-22 15:55:21 +0100

    Dodano: UserAvatar - Avatar użytkownika po zalogowaniu z danymi lub linki do rejestracji i logowania

---
+++ src/components/UserAvatar/UserAvatar.tsx
@@ -0,0 +1,44 @@
+import { useUserLoginStore } from '../../hooks/useUserLoginStore.ts';
+import { Avatar } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
+
+import './UserAvatar.scss';
+import { useLogout } from '../../hooks/useLogout.ts';
+import { useNavigate } from 'react-router';
+
+export const UserAvatar = () => {
+  const { user } = useUserLoginStore();
+
+  const logout = useLogout();
+
+  const navigate = useNavigate();
+
+  if (!user)
+    return (
+      <div className='user-login'>
+        <p className={'login-link'} onClick={() => navigate('/login')}>
+          Logowanie
+        </p>
+
+        <p className={'login-link'} onClick={() => navigate('/register')}>
+          Rejestracja
+        </p>
+      </div>
+    );
+
+  return (
+    <div className='user-avatar'>
+      <div>
+        <p className={'name'}>
+          {user.firstname} {user.lastname}
+        </p>
+        <p className={'card'}>{user.cardNumber}</p>
+        <p className={'login-link'} onClick={logout}>
+          Wyloguj
+        </p>
+      </div>
+
+      <Avatar shape='square' size={64} icon={<UserOutlined />} />
+    </div>
+  );
+};
