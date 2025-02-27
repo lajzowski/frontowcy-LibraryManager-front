0f4bee231d2ab0cf8c18b63a3cd35ffe2ccfa79e
Marcin Woźnica <poczta@marcin.wroclaw.pl>
2025-02-27 20:54:01 +0100

    dodano: test jednostkowe do lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,5 +3,5 @@
  * @type {import('lint-staged').Configuration}
  */
 export default {
-  '*.{ts,tsx}': ['npm run prettify', 'eslint --fix'],
+  '*.{ts,tsx}': ['npm run prettify', 'eslint --fix', 'npm run test'],
 };
