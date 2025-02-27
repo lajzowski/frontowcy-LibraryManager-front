e4f72e63b963b36f127786c4ecc9a10c77161b15
Marcin Woźnica <poczta@marcin.wroclaw.pl>
2025-02-27 20:46:59 +0100

    Refaktoryzacja book.spec.ts

--- tests/book.spec.ts
+++ tests/book.spec.ts
@@ -1,29 +1,18 @@
 import { test } from '@playwright/test';
 
-test('rent book', async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   await page.goto('/login');
   await page.locator('#cardNumber').fill('1111111111');
   await page.locator('#password').fill('testtest1234');
   await page.getByTestId('button-submit').click();
   await page.waitForSelector('.card');
+});
 
-  await page.goto('/books/test-ksiazki');
+test.describe('Book', () => {
+  test('rent book', async ({ page }) => {
+    await page.goto('/books/test-ksiazki');
 
-  const numberText = (
-    await page
-      .locator(
-        'xpath=//*[@id="root"]/div/main/main/div/div/div[2]/div[2]/div/table/tbody/tr[1]/td/span/div/p/span/span[2]/p'
-      )
-      .textContent()
-  )?.replace(/\D/g, '');
-
-  await page.locator('button span:has-text("Wypożycz książkę!")').click();
-
-  await page.locator('button span:has-text("Potwierdzam")').click();
-
-  await page.waitForTimeout(2000);
-  await test.step('Verify the number decreases by one', async () => {
-    const updatedNumberText = (
+    const numberText = (
       await page
         .locator(
           'xpath=//*[@id="root"]/div/main/main/div/div/div[2]/div[2]/div/table/tbody/tr[1]/td/span/div/p/span/span[2]/p'
@@ -31,43 +20,55 @@ test('rent book', async ({ page }) => {
         .textContent()
     )?.replace(/\D/g, '');
 
-    test.expect(numberText).not.toBeNull();
-    test.expect(updatedNumberText).not.toBeNull();
+    await page.locator('button span:has-text("Wypożycz książkę!")').click();
 
-    const previousNumber = parseInt(numberText || '0', 10);
-    const updatedNumber = parseInt(updatedNumberText || '0', 10);
+    await page.locator('button span:has-text("Potwierdzam")').click();
 
-    test.expect(updatedNumber).toBe(previousNumber - 1);
-  });
-});
+    await page.waitForTimeout(2000);
+    await test.step('Verify the number decreases by one', async () => {
+      const updatedNumberText = (
+        await page
+          .locator(
+            'xpath=//*[@id="root"]/div/main/main/div/div/div[2]/div[2]/div/table/tbody/tr[1]/td/span/div/p/span/span[2]/p'
+          )
+          .textContent()
+      )?.replace(/\D/g, '');
 
-test('return book', async ({ page }) => {
-  await test.step('', async () => {
-    await test.step('login process', async () => {
-      await page.goto('/login');
-      await page.locator('#cardNumber').fill('1111111111');
-      await page.locator('#password').fill('testtest1234');
-      await page.getByTestId('button-submit').click();
-      await page.waitForSelector('.card');
+      test.expect(numberText).not.toBeNull();
+      test.expect(updatedNumberText).not.toBeNull();
+
+      const previousNumber = parseInt(numberText || '0', 10);
+      const updatedNumber = parseInt(updatedNumberText || '0', 10);
+
+      test.expect(updatedNumber).toBe(previousNumber - 1);
     });
   });
 
-  await page.goto('/rents');
+  test('return book', async ({ page }) => {
+    await page.goto('/rents');
 
-  await test.step('clicking the hide returned checkbox', async () => {
-    await page.locator('.ant-checkbox-input').click();
-  });
+    await test.step('clicking the hide returned checkbox', async () => {
+      await page.locator('.ant-checkbox-input').click();
+    });
 
-  await test.step('returning the book from the first row', async () => {
-    const key = await page.locator('tr').first().getAttribute('data-row-key');
+    await test.step('returning the book from the first row', async () => {
+      const key = await page.locator('tr').first().getAttribute('data-row-key');
 
-    await page.locator('tbody').locator('tr').first().locator('button').click();
+      await page
+        .locator('tbody')
+        .locator('tr')
+        .first()
+        .locator('button')
+        .click();
 
-    await page.locator('button span:has-text("Potwierdź zwrot")').click();
+      await page.locator('button span:has-text("Potwierdź zwrot")').click();
 
-    await test.step('checking if a row has disappeared from the table', async () => {
-      const rowExists = await page.locator(`tr[data-row-key="${key}"]`).count();
-      test.expect(rowExists).toBe(0);
+      await test.step('checking if a row has disappeared from the table', async () => {
+        const rowExists = await page
+          .locator(`tr[data-row-key="${key}"]`)
+          .count();
+        test.expect(rowExists).toBe(0);
+      });
     });
   });
 });
